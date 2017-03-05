e7a837a42ba2b017af5c2982e399356362e431eb
Nikhil Banerjee <Nikhil.Banerjee5@uk.ibm.com>
2017-03-05 12:36:34 +0000

    OMG SO MUCH THINGS

--- Forms.py
+++ Forms.py
@@ -3,9 +3,7 @@ from wtforms.validators import DataRequired, Length
 
 class SettingsForm(Form):
     #surfaceTemp = IntegerField(widget=NumberInput())
-    surfaceTempField = IntegerField('Surface Temperature (C)', [validators.DataRequired()])
-    seaWaterTempField = IntegerField('Sea Water Temperature (C)', [validators.DataRequired()])
-    precipitationField = IntegerField('Precipitation (mm)', [validators.DataRequired()])
-    cloudCoverageField = IntegerField('Cloud coverage', [validators.DataRequired()])
-    sunDurationField = IntegerField('Sun duration (time)', [validators.DataRequired()])
-    snowThicknessField = IntegerField('Snow Thickness (cm)', [validators.DataRequired()])
+    surfaceTempField = StringField('Surface Temperature')
+    seaWaterTempField = StringField('Sea Water Temperature')
+    cloudCoverageField = StringField('Cloud coverage')
+    snowThicknessField = StringField('Snow Thickness (cm)')
